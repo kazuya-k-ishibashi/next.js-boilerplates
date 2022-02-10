1a2aa68c4cd83964a0a61982ce73ae453c6ec16a
kazuya-k-ishibashi <kazuya.ishibashi.wn@gmail.com>
2022-02-10 23:31:54 +0900

    基本構成をカスタマイズした

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import type { AppProps } from 'next/app'
+
+const CustomApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default CustomApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from 'next'
+import { IndexPage } from '@/client/components/pages/IndexPage'
+
+const IndexPageContainer: NextPage = () => <IndexPage />
+
+export default IndexPageContainer
