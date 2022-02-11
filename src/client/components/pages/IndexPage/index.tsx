12b6e9a3ced44ce4df6c18ff8eaf5283888abc58
kazuya-k-ishibashi <kazuya.ishibashi.wn@gmail.com>
2022-02-11 23:11:17 +0900

    Emotionを追加した

--- src/client/components/pages/IndexPage/index.tsx
+++ src/client/components/pages/IndexPage/index.tsx
@@ -1,3 +1,25 @@
 import React from 'react'
+import { css, useTheme } from '@emotion/react'
 
-export const IndexPage: React.FC = () => <h1>Hello, Next.js!</h1>
+export const IndexPage: React.FC = () => {
+  const styles = useStyles()
+
+  return <h1 css={styles.title()}>Hello, Next.js!</h1>
+}
+
+const useStyles = () => {
+  const { breakpoints } = useTheme()
+
+  const title = () =>
+    css({
+      fontSize: '2rem',
+      fontWeight: 'bold',
+      [breakpoints.mediumUp]: {
+        textDecoration: 'underline',
+      },
+    })
+
+  return {
+    title,
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, Global } from '@emotion/react'
+import { theme } from '@/client/styles/theme'
+import { globalStyles } from '@/client/styles/global'
+import 'destyle.css'
 
 const CustomApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default CustomApp
